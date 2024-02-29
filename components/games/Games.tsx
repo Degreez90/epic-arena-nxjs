30b1e5675d89cfae19c0884640b9ab25be8e9683
Offitt <offitt90@gmail.com>
2024-02-29 16:12:08 -0500

    clerk webhook added

--- components/games/Games.tsx
+++ components/games/Games.tsx
@@ -3,6 +3,8 @@ import Container from '../Container'
 import tourneydata from '@/app/utils/games'
 import Gamelist from './Gamelist'
 
+console.log('this web burn 5656 67676 wergdf')
+
 const Games = () => {
   return (
     <Container>
