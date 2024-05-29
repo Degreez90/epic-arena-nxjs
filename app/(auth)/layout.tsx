e18c629532529220d7f7329836e7f0900c4d8c9d
Offitt <offitt90@gmail.com>
2024-05-29 06:37:31 -0400

    Changed form schema for Register

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className='flex items-center justify-center h-full w-full bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-cyan-500 to-blue-500'>
+    <div className='flex items-center justify-center h-full w-full bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-indigo-300 to-sky-950'>
       {children}
     </div>
   )
--- schemas/index.ts
+++ schemas/index.ts
@@ -2,12 +2,31 @@ import * as z from 'zod'
 
 export const RegisterSchema = z.object({
   email: z.string().email({
-    message: 'Email is required',
+    message: 'Invalid email format',
   }),
   password: z.string().min(6, {
-    message: 'Minimum 6 characters required',
+    message: 'Password must be at least 6 characters long',
   }),
-  name: z.string().min(1, {
-    message: 'Name is required',
+  firstName: z.string().min(1, {
+    message: 'First name is required',
   }),
+  lastName: z.string().min(1, {
+    message: 'Last name is required',
+  }),
+  phoneNumber: z.string().regex(/^\+?[1-9]\d{1,14}$/, {
+    message: 'Invalid phone number format',
+  }),
+  date: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, {
+    message: 'Date must be in the format YYYY-MM-DD',
+  }),
+})
+
+export const LoginSchema = z.object({
+  email: z.string().email({
+    message: 'Email is required',
+  }),
+  password: z.string().min(1, {
+    message: 'Password is required',
+  }),
+  code: z.optional(z.string()),
 })
