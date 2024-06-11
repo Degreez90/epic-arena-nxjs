252d244670fe02bdc0fd7c1954416c436d0680b2
Offitt <offitt90@gmail.com>
2024-06-11 00:15:23 -0400

    Changes to middleware.ts

---
+++ next-auth.d.ts
@@ -0,0 +1,16 @@
+import { UserRole } from '@prisma/client'
+import NextAuth, { type DefaultSession } from 'next-auth'
+
+import { UserType } from './models/User'
+
+export type ExtendedUser = DefaultSession['user'] & {
+  role: UserRole
+  isTwoFactorEnabled: boolean
+  isOAuth: boolean
+}
+
+declare module 'next-auth' {
+  interface Session {
+    user: UserType
+  }
+}
