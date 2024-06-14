47432645b1aac891daa05e389a09c49841b54442
Offitt <offitt90@gmail.com>
2024-06-14 05:10:36 -0400

    type changes

--- data/user.ts
+++ data/user.ts
@@ -1,4 +1,5 @@
 import clientPromise from '@/lib/db'
+import { UserType } from '@/models/User'
 
 export const getUserByEmail = async (email: string) => {
   try {
@@ -6,6 +7,19 @@ export const getUserByEmail = async (email: string) => {
     const db = client.db()
     const collection = db.collection('users')
     const user = await collection.findOne({ email })
+
+    return user
+  } catch {
+    return null
+  }
+}
+
+export const getUserById = async (id?: string) => {
+  try {
+    const client = await clientPromise
+    const db = client.db()
+    const collection = db.collection('users')
+    const user = await collection.findOne({ id })
 
     return user
   } catch {
--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,16 +1,14 @@
-import { UserRole } from '@prisma/client'
 import NextAuth, { type DefaultSession } from 'next-auth'
 
 import { UserType } from './models/User'
 
 export type ExtendedUser = DefaultSession['user'] & {
-  role: UserRole
   isTwoFactorEnabled: boolean
   isOAuth: boolean
 }
 
 declare module 'next-auth' {
   interface Session {
-    user: UserType
+    user: ExtendedUser
   }
 }
