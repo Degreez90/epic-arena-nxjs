a74a0f17be0eecb298d058bb237ce38eb0a85471
Mark Offitt <offitt@gmail.com>
2024-06-15 21:07:26 -0400

    Added prisma

--- auth.config.ts
+++ auth.config.ts
@@ -18,8 +18,8 @@ export default {
         if (validatedFields.success) {
           const { email, password } = validatedFields.data
 
-          // const user = await getUserByEmail(email)
-          // if (!user || !user.password) return null
+          const user = await getUserByEmail(email)
+          if (!user || !user.password) return null
 
           const passwordsMatch = await bcrypt.compare(password, user.password)
           if (passwordsMatch) return user
