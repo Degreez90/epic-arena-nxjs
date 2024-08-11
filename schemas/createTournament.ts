21bdd12cce128b1c099e9a2ef40e5ba83ef128d1
Offitt <offitt90@gmail.com>
2024-08-10 21:01:02 -0400

    added bracket models

--- schemas/createTournament.ts
+++ schemas/createTournament.ts
@@ -3,7 +3,7 @@ import * as z from 'zod'
 export const CreateTournamentSchema = z.object({
   tName: z.string().min(1, { message: 'Tournament name is required' }),
   description: z.string().min(1, { message: 'Tournament name is required' }),
-  type: z.enum(['Single Elimintation', 'Double Elimination'], {
+  type: z.enum(['single_elimination', 'double_elimination', 'round_robin'], {
     message: 'Tournament type is required',
   }),
   thirdPlaceMatch: z.boolean().default(false),
