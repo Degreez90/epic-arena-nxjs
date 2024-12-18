f2cdee6be5c8b3de1b9e07b1e8923e4d0f975c56
Mark Offitt <offitt@gmail.com>
2024-12-18 18:07:27 -0500

    Added admin to register.ts

--- actions/tournament/create-tournament.ts
+++ actions/tournament/create-tournament.ts
@@ -2,24 +2,25 @@
 import { db } from '@/lib/db'
 import type { CreateTournamentType } from '@/schemas/createTournament'
 import { CreateTournamentSchema } from '@/schemas/createTournament'
+import { Prisma } from '@prisma/client'
 
 //TODO:: finish create tounament
 const createTournament = async (data: CreateTournamentType) => {
   const validatedData = CreateTournamentSchema.parse(data)
 
-  const tournament = await db.tournament.create({
-    data: {
-      name: validatedData.tName,
-      description: validatedData.description,
-      status: {
-        create: [
-          {
-            type: validatedData.type,
-          },
-        ],
-      },
+  const tournament: Prisma.TournamentCreateInput = {
+    name: validatedData.tName,
+    description: validatedData.description,
+    stages: {
+      create: [
+        {
+          type: validatedData.type,
+          number: 1,
+          name: '',
+        },
+      ],
     },
-  })
+  }
 }
 
 export default createTournament
