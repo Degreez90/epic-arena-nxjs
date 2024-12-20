8b3fbeadb0c1e1f69afc3965d1584a941fd1c093
Mark Offitt <offitt90@gmail.com>
2024-12-20 18:35:50 -0500

    Added User

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,9 @@
-export { GET, POST } from '@/auth'
+//Original code:
+// export { GET, POST } from '@/auth'
+
+// Modified code:
+
+import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth";
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
