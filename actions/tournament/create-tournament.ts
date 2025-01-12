662ae9508b78f09d9a1486a4e37dee4e6d21f3f9
Mark Offitt <offitt90@gmail.com>
2025-01-12 10:35:22 -0500

    Enhance createTournament function to include user information and rename tournamentName field for clarity

--- actions/tournament/create-tournament.ts
+++ actions/tournament/create-tournament.ts
@@ -3,18 +3,23 @@ import { db } from '@/lib/db'
 import type { CreateTournamentType } from '@/schemas/createTournament'
 import { CreateTournamentSchema } from '@/schemas/createTournament'
 import { Prisma } from '@prisma/client'
+import { User } from 'next-auth'
 
 //TODO:: finish create tounament
-export const createTournament = async (data: CreateTournamentType) => {
+export const createTournament = async (
+  data: CreateTournamentType,
+  user: User
+) => {
   const validatedData = CreateTournamentSchema.parse(data)
 
   const tournament = {
     _id: Date.now().toString(),
-    tName: validatedData.tName,
+    tournamentName: validatedData.tName,
     description: validatedData.description,
     type: validatedData.type,
     thirdPlaceMatch: validatedData.thirdPlaceMatch,
     seedOrdering: validatedData.seedOrdering,
+    createdBy: user.id,
   }
 }
 
